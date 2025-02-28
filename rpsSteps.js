5ef12411bf89b4446320dd5564a1313ed4bdb787
Wilmer <25WilmerL@badinhs.org>
2025-02-28 13:36:19 -0500

    added another tab thing for the steps

---
+++ rpsSteps.js
@@ -0,0 +1,36 @@
+// "List of steps for project"
+
+/* Steps:
+1. You need to make a new GitHub repo for this. 
+In that repo you will need index.html, and rps.js
+
+2. Copy the index.html code from an old repo, make the script link to rps.js
+
+3.  Save your work to GitHub (git status -> git add -> git commit -> git push origin main)
+
+4. In rps.js, you are going to make "pseudocode" for your game.  Write out every 
+step that you need to do to play the game.  We're going to use multiple functions 
+to play the game, so you'll to write each of these functions.
+
+5.  Once you've completed your pseudocode, commit rps.js to GitHub (commit -m "completed pseudocode")
+
+6.  write the actual code.  (I'd commit after you finish each function)
+
+7.  When it's all said and done, I will help you get GitHub to host this webpage, 
+so that you can play it from anywhere.
+
+Here are some things that might help you:
+1 function should be getting the computer's choice for that round
+(Math.random() lets you get a random number, you can use stuff to help that choose between 3 choices)
+
+1 function should get the human's choice.  (prompt() is a good option here).
+
+when we're completely done with this project, it will be a first player to 
+5 wins version of the game.  Make variables to keep track of the score.
+
+Once you've written the functions to get choices, write code to play just 
+one round, and make sure that's working properly, before you expand to the first 5 version.  
+
+MAKE SURE:  when the winner is declared, it should give a message like "You win!  Paper beats rock!"  
+Or "You lose!  Scissors beats paper!"  Messages like "Sorry, you lose" or "Congrats you win", don't 
+make your player trust that the program is fair.
