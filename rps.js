4ab8c38cda2fad8e61beec5351d3fc4dfe19ad6d
Wilmer <25WilmerL@badinhs.org>
2025-03-03 13:47:30 -0500

    worked on the rps

--- rps.js
+++ rps.js
@@ -1,2 +1,32 @@
 // This is for rock paper scissors project;
 
+
+const rock = 1;
+const paper = 2;
+const scissors = 3;
+let PlayerChoice = undefined;
+let BotChoice = undefined;
+let input = undefined;
+function Playerinput(){
+    input = prompt("Rock, Paper, Scissors");
+
+switch(input){
+    case "Rock":
+        PlayerChoice = 1;
+        break;
+    case "Paper":
+        PlayerChoice = 2;
+        break;
+    case "Scissors":
+        PlayerChoice = 3;
+        break;
+    default:
+        Playerinput()
+        break;
+    }
+}
+Playerinput()
+function Botinput(max){
+    BotChoice = Math.ceil(Math.random() * max)
+    return BotChoice
+}
--- rpsSteps.js
+++ rpsSteps.js
@@ -1,23 +1,23 @@
 // "List of steps for project"
 
-/* Steps:
-1. You need to make a new GitHub repo for this. 
-In that repo you will need index.html, and rps.js
+ Steps:
+1. /*You need to make a new GitHub repo for this. 
+In that repo you will need index.html, and rps.js*/
 
-2. Copy the index.html code from an old repo, make the script link to rps.js
+2. /*Copy the index.html code from an old repo, make the script link to rps.js*/
 
-3.  Save your work to GitHub (git status -> git add -> git commit -> git push origin main)
+3.  /*Save your work to GitHub (git status -> git add -> git commit -> git push origin main)*/
 
-4. In rps.js, you are going to make "pseudocode" for your game.  Write out every 
+4. /*In rps.js, you are going to make "pseudocode" for your game.  Write out every 
 step that you need to do to play the game.  We're going to use multiple functions 
-to play the game, so you'll to write each of these functions.
+to play the game, so you'll to write each of these functions.*/
 
-5.  Once you've completed your pseudocode, commit rps.js to GitHub (commit -m "completed pseudocode")
+5.  /*Once you've completed your pseudocode, commit rps.js to GitHub (commit -m "completed pseudocode")*/
 
-6.  write the actual code.  (I'd commit after you finish each function)
+6.  /*write the actual code.  (I'd commit after you finish each function)*/
 
-7.  When it's all said and done, I will help you get GitHub to host this webpage, 
-so that you can play it from anywhere.
+7.  /*When it's all said and done, I will help you get GitHub to host this webpage, 
+so that you can play it from anywhere.*/
 
 Here are some things that might help you:
 1 function should be getting the computer's choice for that round
