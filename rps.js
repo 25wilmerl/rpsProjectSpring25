04d413e67efa5471e5acb5c51d37da81f630791f
Wilmer <25WilmerL@badinhs.org>
2025-03-06 13:32:54 -0500

    finsihed the final project and it is working

--- rps.js
+++ rps.js
@@ -12,15 +12,15 @@ let input = undefined;
 
 function Playerinput(){
     input = prompt("Rock, Paper, Scissors");
-
+input = input.toLowerCase();
     switch(input){
-        case "Rock":
+        case "rock":
             PlayerChoice = 1;
             break;
-        case "Paper":
+        case "paper":
             PlayerChoice = 2;
             break;
-        case "Scissors":
+        case "scissors":
             PlayerChoice = 3;
             break;
         default:
@@ -41,7 +41,7 @@ function Botinput(max){
         console.log("You tied, I hope you do better next time")
     } else if(PlayerChoice == Rock){
         if(BotChoice == Scissors){
-            PlayerChoice = Playerscore +1;
+            Playerscore = Playerscore +1;
             console.log("The Bot picked scissors, you won:)");
         } else {
             Botscore = Botscore +1;
@@ -50,7 +50,7 @@ function Botinput(max){
     }
     else if(PlayerChoice == Paper){
         if(BotChoice == Rock){
-            PlayerChoice = Playerscore +1;
+            Playerscore = Playerscore +1;
             console.log("The Bot picked rock, you win ;)");
         } else {
             Botscore = Botscore +1;
@@ -58,19 +58,19 @@ function Botinput(max){
         }
     } else if(PlayerChoice == Scissors){
         if(BotChoice == Paper){
-            PlayerChoice = Playerscore +1;
+            Playerscore = Playerscore +1;
             console.log("The Bot picked rock, you win :)");
         } else {
             Botscore = Botscore +1;
             console.log("You lost, the Bot chose paper, try again :(");
         }
-    } console.log(Playerscore);
-    console.log(Botscore);
+    } console.log(`Playerscore is ${Playerscore}`);
+    console.log(`Botscore is ${Botscore}`);
 }
 
-while (Playerscore < 3 && Botscore < 3){
+while (Playerscore < 5 && Botscore < 5){
     Botinput(3);
 }
-while (Botscore < 3 && Playerscore < 3){
+while (Botscore < 5 && Playerscore < 5){
     Playerinput(3);
-}
+};
